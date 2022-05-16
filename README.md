dac00d6dd95aa859adc6d89db0103006bda3f287
Brian Gershon <briangershon@users.noreply.github.com>
2022-05-16 15:04:22 -0700

    Mock up UI

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 An open-source web-based Clicker style game running on the Solana blockchain.
 
+Front-end technology:
+
+- Next.js
+- tailwindcss
+
 ## Running locally
 
 See [README.md](web-ui/README.md) to run the web frontend.
--- web-ui/pages/index.tsx
+++ web-ui/pages/index.tsx
@@ -6,9 +6,39 @@ const Home: NextPage = () => {
   return (
     <div className="flex items-center flex-col">
       <Head>
-        <title>Home</title>
+        <title>Solana Clicker</title>
       </Head>
-      <h1 className="text-3xl font-bold">Welcome</h1>
+
+      <div className="flex flex-col sm:flex-row">
+        <div className="p-10 flex flex-col items-center justify-between">
+          <h2 className="text-xl font-bold">Solana Clicker</h2>
+          <div className="flex flex-col items-center p-2">
+            <div>0 clicks</div>
+            <div>0 cps</div>
+          </div>
+          <button className="bg-blue-500 p-2 h-48 w-48 rounded-full">
+            Click Me
+          </button>
+        </div>
+
+        <div className="sm:p-10 items-center flex flex-col justify-between">
+          <h2 className="text-xl font-bold">Extras</h2>
+          <div className="bg-orange-300 m-3 border-0 p-2 shadow-md w-48">
+            auto-clicker one
+          </div>
+          <div className="bg-pink-300 m-3 border-0 p-2 shadow-md w-48">
+            auto-clicker two
+          </div>
+          <div className="bg-green-300 m-3 border-0 p-2 shadow-md w-48">
+            auto-clicker three
+          </div>
+        </div>
+      </div>
+      <div className="p-2 m-3 flex">
+        <div className="bg-blue-300 m-3 w-28 h-16 shadow p-2">bonus one</div>
+        <div className="bg-blue-300 m-3 w-28 h-16 shadow p-2">bonus two</div>
+        <div className="bg-blue-300 m-3 w-28 h-16 shadow p-2">bonus three</div>
+      </div>
     </div>
   );
 };
