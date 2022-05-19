2340b1e1be5a231464a51245d8d5ab008ebd313f
Brian Gershon <briangershon@users.noreply.github.com>
2022-05-19 09:41:44 -0700

    Add Solana wallet adapter (and their styling)

--- web-ui/pages/index.tsx
+++ web-ui/pages/index.tsx
@@ -2,6 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 // import Image from "next/image";
 import { useState } from "react";
+import {
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 
 const Home: NextPage = () => {
   const [clicks, setClicks] = useState(0);
@@ -17,32 +21,33 @@ const Home: NextPage = () => {
         <title>Solana Clicker</title>
       </Head>
 
-      <div className="flex flex-col sm:flex-row">
-        <div className="p-10 flex flex-col items-center justify-between">
-          <h2 className="text-xl font-bold">Solana Clicker</h2>
-          <div className="flex flex-col items-center p-2">
-            <div
-              onAnimationEnd={() => {
-                setEffect(false);
-              }}
-              className={`${effect && "animate-wiggle"}`}
-            >
-              {clicks} clicks
+      <div>
+        <div className="flex flex-col sm:flex-row">
+          <div className="p-10 flex flex-col items-center justify-between">
+            <h2 className="text-xl font-bold">Solana Clicker</h2>
+            <div className="flex flex-col items-center p-2">
+              <div
+                onAnimationEnd={() => {
+                  setEffect(false);
+                }}
+                className={`${effect && "animate-wiggle"}`}
+              >
+                {clicks} clicks
+              </div>
+              {/* <div>0 cps</div> */}
             </div>
-            {/* <div>0 cps</div> */}
+            <button
+              onClick={() => {
+                handleClick();
+                setEffect(true);
+              }}
+              className="bg-blue-400 border-blue-600 border-4 hover:bg-blue-600 hover:text-white p-2 h-48 w-48 rounded-full"
+            >
+              Click Me
+            </button>
           </div>
-          <button
-            onClick={() => {
-              handleClick();
-              setEffect(true);
-            }}
-            className="bg-blue-400 border-blue-600 border-4 hover:bg-blue-600 hover:text-white p-2 h-48 w-48 rounded-full"
-          >
-            Click Me
-          </button>
-        </div>
 
-        {/* <div className="sm:p-10 items-center flex flex-col justify-between">
+          {/* <div className="sm:p-10 items-center flex flex-col justify-between">
           <h2 className="text-xl font-bold">Extras</h2>
           <div className="bg-orange-300 m-3 border-0 p-2 shadow-md w-48">
             auto-clicker one
@@ -54,12 +59,22 @@ const Home: NextPage = () => {
             auto-clicker three
           </div>
         </div> */}
-      </div>
-      {/* <div className="p-2 m-3 flex">
+        </div>
+        {/* <div className="p-2 m-3 flex">
         <div className="bg-blue-300 m-3 w-28 h-16 shadow p-2">bonus one</div>
         <div className="bg-blue-300 m-3 w-28 h-16 shadow p-2">bonus two</div>
         <div className="bg-blue-300 m-3 w-28 h-16 shadow p-2">bonus three</div>
       </div> */}
+      </div>
+
+      <div className="flex gap-2">
+        <div className="bg-slate-300 p-2 border-2 rounded-lg">
+          <WalletMultiButton />
+        </div>
+        <div className="bg-slate-700 p-2 border-2 rounded-lg">
+          <WalletDisconnectButton />
+        </div>
+      </div>
     </div>
   );
 };
